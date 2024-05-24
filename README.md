b6932e9c91df1a43d616081910cffb6ccea2bfc3
園田 亮平 <ryohei-sonoda@m3.com>
2024-05-25 02:37:56 +0900

    initialize

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # dify-aws-terraform
+
 Terraform template for Dify on AWS
+
+## Prerequisites
+
+- Terraform
+
+## Usage
+
+1. Clone this repository
+2. Edit `terraform.tfvars` to set your variables
+3. Edit `backend.tf` to set your S3 bucket and DynamoDB table
+4. Run `terraform init`
+5. Run `terraform plan`
+6. Run `terraform apply -target aws_elasticache_subnet_group.redis`
+7. Run `terraform apply -target aws_rds_cluster_instance.dify`
+8. Run `terraform apply -target aws_lb_listener_rule.api`
+9. Run `terraform apply`
---
+++ variables.tf
@@ -0,0 +1,81 @@
+# AWS Provider
+
+variable "aws_region" {}
+
+variable "default_tags" {
+  type = map(string)
+}
+
+# S3 Bucket for Dify Storage
+
+variable "dify_storage_bucket" {
+  description = "s3 bucket name for dify storage"
+}
+
+# VPC
+
+variable "vpc_id" {
+}
+variable "private_subnet_ids" {
+  type = list(string)
+}
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+# Redis
+
+variable "redis_password" {
+  default   = "redis_dummy_auth_token"
+  sensitive = true
+  # 初回実行時は dummy で実行し、構築後に以下のコマンドで更新する。
+  # aws elasticache modify-replication-group \
+  # --replication-group-id replication-group-sample \
+  # --auth-token new-token \
+  # --auth-token-update-strategy SET \
+  # --apply-immediately
+}
+
+# Database
+
+variable "db_master_password" {
+  default   = "dummy" # 初回実行時に TF_VAR_db_master_password=xxx で与える
+  sensitive = true
+}
+
+# Dify environment
+
+variable "dify_api_version" {
+  default = "0.6.8"
+}
+
+variable "dify_web_version" {
+  default = "0.6.8"
+}
+
+variable "dify_sandbox_version" {
+  default = "0.2.0"
+}
+
+variable "migration_enabled" {
+  default = "false"
+}
+
+variable "dify_db_username" {
+  default = "dify"
+}
+variable "dify_db_password" {
+  default   = "dummy"
+  sensitive = true
+}
+variable "dify_db_name" {
+  default = "dify"
+}
+
+# ALB
+
+variable "allowed_cidr_blocks" {
+  type    = list(string)
+  default = []
+}
+
